c1e93fadd4fa4b5bff839aca1d054c30018c8579
shamanlvlgod <arto.hovakimyan@gmail.com>
2025-03-22 19:14:46 +0100

    added OnBeforeGet and OnBeforeRelease

--- Runtime/IPool.cs
+++ Runtime/IPool.cs
@@ -1,8 +1,20 @@
+using System;
+
 namespace Utils
 {
     public interface IPool<T>
     {
         T Get();
         void Release(T item);
+        
+        /// <summary>
+        /// Optional hook called before an item is returned to the pool.
+        /// </summary>
+        Action<T> OnBeforeReturn { get; set; }
+
+        /// <summary>
+        /// Optional hook called after an item is retrieved from the pool.
+        /// </summary>
+        Action<T> OnBeforeGet { get; set; }
     }
 }
--- Runtime/Pool.cs
+++ Runtime/Pool.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 
 namespace Utils
 {
-
-    
     public class Pool<T> : IPool<T>
     {
         private readonly Stack<T> _stack = new();
 
         public Func<T> CreateFunc;
-
+        public Action<T> OnBeforeGet { get; set; }
+        public Action<T> OnBeforeReturn { get; set; }
         public Pool(Func<T> createFunc = null, int preload = 0)
         {
             CreateFunc = createFunc ?? throw new ArgumentNullException(nameof(createFunc));
@@ -25,13 +24,21 @@ namespace Utils
         public T Get()
         {
             T item = _stack.Count > 0 ? _stack.Pop() : Create();
-            if (item is IPoolable p) p.OnTakenFromPool();
+            OnBeforeGet?.Invoke(item);
+            if (item is IPoolable p)
+            {
+                p.OnTakenFromPool();
+            }
             return item;
         }
 
         public void Release(T item)
         {
-            if (item is IPoolable p) p.OnReturnedToPool();
+            if (item is IPoolable p)
+            {
+                p.OnReturnedToPool();
+            }
+            OnBeforeReturn?.Invoke(item);
             _stack.Push(item);
         }
 
